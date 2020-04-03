199b530bfbde4da9247ed47b9c072314e051dba8
Gordiig <gordiig@gmail.com>
2020-04-03 12:36:31 +0300

    Add __head to __main__

--- 2_Lab/main.py
+++ 2_Lab/main.py
@@ -2,6 +2,13 @@ from grammar import Grammar, GrammarException
 
 
 if __name__ == '__main__':
+    __head = """
+    ИУ7-23М
+    Горин Дмитрий
+    Вариант 3 (удаление eps-правил)
+    """
+    print(__head)
+
     g1 = Grammar.init_from_json_file('test_grammar.json')
     print(g1)
     print(g1.eps_rules)
@@ -14,4 +21,5 @@ if __name__ == '__main__':
 
     if g2.has_circuits():
         print('Грамматика без eps-правил содержит цепи, построить новую без левой рекурсии не получится')
+        print('Вариант такой, я не виноват')
         exit(0)
