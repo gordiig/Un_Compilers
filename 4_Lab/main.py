05fef2e9766fc576506744555cdfa03f46ab8636
Gordiig <gordiig@gmail.com>
2020-05-16 12:07:14 +0300

    Add all

--- 4_Lab/main.py
+++ 4_Lab/main.py
@@ -3,7 +3,26 @@ from analyzer import analyze_string
 
 if __name__ == '__main__':
 
-    input_str = '(1 <> 3) < (2 + 3) / 4'
+    input_str = '1 < 2'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 12<')
 
-    ans = analyze_string(input_str=input_str)
-    print(ans)
+    input_str = '3 <> 4'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 34<>')
+
+    input_str = '1 + 2 < 3 - 4'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 12+34-<')
+
+    input_str = '1 + 2 < 3 + 4 / 5'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 12+345/+<')
+
+    input_str = '1 + 2 < (3 + 4) / 5'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 12+34+5/<')
+
+    input_str = '(1 <> 3) < (2 + 3) / 4'
+    ans = analyze_string(input_str)
+    print(f'Ans is {ans}, should be: 13<>23+4/<')
